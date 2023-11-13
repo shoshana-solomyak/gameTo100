e0009e46cef13271e0c61371d042271c63a83f1c
brachalevi <barlevi055@gmail.com>
2023-11-13 15:54:32 +0200

    register and in game

--- gameTo100/src/App.jsx
+++ gameTo100/src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import GameCenter from "./components/GameCenter";
 import Register from "./components/Register";
+import UserList from "./components/UserList";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [inGame, setInGame] = useState(false);
+
   return (
     <>
-      <GameCenter />
-
-      <Register users={users} setUsers={setUsers} />
+      {inGame ? (
+        <GameCenter />
+      ) : (
+        <>
+          <Register users={users} setUsers={setUsers} />{" "}
+          <UserList setInGame={setInGame} users={users} />
+        </>
+      )}
     </>
   );
 }
