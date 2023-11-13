c9e6b9c1695e592b63c927efa98281152e0d6081
shoshana-solomyak <shoshanasolomyak@hilma.tech>
2023-11-13 16:35:23 +0200

    update

--- gameTo100/src/components/GameTo100.jsx
+++ gameTo100/src/components/GameTo100.jsx
@@ -9,54 +9,37 @@ function GameTo100() {
       setWon("you won!");
       return true;
     }
+    return false;
   }
   function newGame() {
     setScore(0);
     setWon("");
     setNum(Math.floor(Math.random() * 100));
   }
-
+  function handleClick(action) {
+    let newNum;
+    if (action === "+") {
+      newNum = num + 1;
+    } else if (action === "-") {
+      newNum = num - 1;
+    } else if (action === "*") {
+      newNum = num * 2;
+    } else if (action === "/") {
+      newNum = num / 2;
+    }
+    checkWin(newNum);
+    setScore(score + 1);
+    setNum(newNum);
+  }
   return (
     <div className="game">
       {num}
       <p>score : {score}</p>
       <div id="buttons">
-        <button
-          onClick={() => {
-            setNum(num + 1);
-            setScore(score + 1);
-            checkWin(num + 1);
-          }}
-        >
-          +1
-        </button>
-        <button
-          onClick={() => {
-            setNum(num - 1);
-            setScore(score + 1);
-            checkWin(num - 1);
-          }}
-        >
-          -1
-        </button>
-        <button
-          onClick={() => {
-            setNum(num * 2);
-            setScore(score + 1);
-            checkWin(num * 2);
-          }}
-        >
-          x2
-        </button>
-        <button
-          onClick={() => {
-            setNum(Math.floor(num / 2));
-            setScore(score + 1);
-            checkWin(Math.floor(num / 2));
-          }}
-        >
-          :2
-        </button>
+        <button onClick={() => handleClick("+")}>+1</button>
+        <button onClick={() => handleClick("-")}>-1</button>
+        <button onClick={() => handleClick("*")}>x2</button>
+        <button onClick={() => handleClick("/")}>/2</button>
       </div>
       {won && <p>{won}</p>}
       {won && <button onClick={newGame}>new game</button>}
